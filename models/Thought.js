1baca7cda729b38d6c084cdec21a866881762c5e
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-02-19 21:57:28 -0600

    Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -2,6 +2,36 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const moment = require('moment');
 
+const thoughtSchema = new schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAt) => moment(createdVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+     reations: [reationSchema]     
+},
+{
+    toJSON: {
+       virtuals: true,
+       getters: true 
+    },
+    id: false
+});
 
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
